bb79bea5a12686b94e5fb9615a9b0f4800d97411
Hendrik <67455442+Hendrikjilderda@users.noreply.github.com>
2021-08-12 16:51:08 +0200

    werkende CP plots

--- scripts/explainer_objects.R
+++ scripts/explainer_objects.R
@@ -1,6 +1,8 @@
 
 train <- GCR_train
 
+e_data <- custom_data_expl(model_fitted, train, "Risk")
+
 .GlobalEnv$explainer_randomforest <- DALEX::explain(
   model = custom_model_expl(model_fitted),
   data = custom_data_expl(model_fitted, train, "Risk"),
@@ -11,3 +13,31 @@ train <- GCR_train
                protected = GCR_train$Sex,
                privileged = "male",
                cutoff = 0.5)
+
+
+# todo:
+# maken van functies voor fairness 
+# bv. heatmap, boxplots etc -> check fiarness bladwijzer
+
+# verder nog testn met het explainability script!
+
+case <- e_data[sample(1:nrow(e_data),1),]
+
+SHAP_val <- DALEX::predict_parts(
+  explainer = explainer_randomforest,
+  new_observation = case,
+  type = "shap"
+)
+plot(SHAP_val)
+
+
+CP_val <- ingredients::ceteris_paribus(x = explainer_randomforest,
+                                       new_observation = case)
+
+plot(CP_val)
+
+VIP_Val <- DALEX::model_parts(
+  explainer = explainer_randomforest,
+  loss_function = DALEX::loss_root_mean_square
+)
+plot(VIP_Val)
