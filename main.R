e4cfd61a5c86f8b96b66f81e6c7330aa87523ec7
Hendrik <67455442+Hendrikjilderda@users.noreply.github.com>
2021-08-16 10:44:01 +0200

    pictures voor readme

--- main.R
+++ main.R
@@ -8,10 +8,22 @@ source('scripts/randomforest.R')
 
 
 source('scripts/explainability/functions.R')
-make_vars(model_fitted2, GCR_train, "Risk", label = 'RandomForest', PDP_variable = 'Job', seed = 123)   
-source('scripts/explainability/script.R') #plots printen nog niet direct. moeten lost geprint worden
+make_vars(model_fitted2, GCR_train, "Risk", label = 'RandomForest', PDP_variable = 'Job', seed = 2121)   
+source('scripts/explainability/script.R') 
 
 
-source('scripts/fairness/functions.R')
-make_fairness_vars(explainer, GCR_train$Sex, "male", 0.5)
-source('scripts/fairness/script.R')
+# source('scripts/fairness/functions.R')
+# make_fairness_vars(explainer, GCR_train$Sex, "male", 0.5)
+# source('scripts/fairness/script.R')
+
+
+
+plot(plot_SHAP)
+
+plot(plot_BD)
+
+plot(plot_CP)
+
+plot(plot_VIP)
+
+plot(plot_PDP)
