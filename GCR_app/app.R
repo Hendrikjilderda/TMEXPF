ee3968f85be9ed5458d7578630cda0438f64ce05
Hendrik <67455442+Hendrikjilderda@users.noreply.github.com>
2021-08-19 15:26:35 +0200

    breakdown plot added to app

--- GCR_app/app.R
+++ GCR_app/app.R
@@ -5,8 +5,11 @@ library(shiny)
 library(shinydashboard)
 library(tidymodels)
 library(tidyverse)
+library(iBreakDown)
 
 model  <- readRDS("GCR_xgmodel.rds")
+explainer <- readRDS("GCR_xg_explainer.rds")
+recipe <- readRDS("GCR_xg_rec.rds")
 
 # testcase
 
@@ -52,6 +55,8 @@ ui <- dashboardPage(skin = "blue",
         tabItem(
             tabName = "GCR tab",
 
+            plotOutput("BD_plot"),
+            
             box(valueBoxOutput("GCR_prediction", width = 12), width = 12),
             
             box(selectInput("v_purpose", label = "Purpose",
@@ -83,10 +88,10 @@ ui <- dashboardPage(skin = "blue",
             
             box(sliderInput("v_age", label = "Age",
                             min = 19, max = 75, value = 35)
-            ,width = 4),
+            ,width = 12),
             box(sliderInput("v_creditamount", label = "Credit amount",
                             min = 276, max = 18242, value = 2300)
-            ,width = 4),
+            ,width = 12),
             box(sliderInput("v_duration", label = "Duration",
                             min = 6, max = 72, value = 18)
             ,width = 4)    
@@ -96,6 +101,30 @@ ui <- dashboardPage(skin = "blue",
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    output$BD_plot <- renderPlot({ 
+
+      new_data <- tibble("Age" = input$v_age,
+             "Sex" = input$v_sex,
+             "Job" = input$v_job,
+             "Housing" = input$v_housing,
+             "Saving.accounts" = input$v_savings,
+             "Checking.account" = input$v_checkings,
+             "Credit.amount" = input$v_creditamount,
+             "Duration" = input$v_duration,
+             "Purpose" = input$v_purpose,
+             "X" = 0)
+      
+      prep <- prep(recipe, verbose = TRUE)
+      
+      case <- bake(prep, new_data)
+      
+      BD_plot <- iBreakDown::local_attributions(explainer, 
+                                                    case, 
+                                                    keep_distributions = TRUE)
+      
+      plot(BD_plot)
+    })
+  
     output$GCR_prediction <- renderValueBox({
         
         prediction <- predict(
@@ -132,12 +161,12 @@ server <- function(input, output) {
             
             
         prediction_colour <- if_else(prediction$.pred_class == 1, "green", "red")
-        
+
         valueBox(
             value = paste0(round(100*prob_prediction, 0), "%"),
             subtitle = paste0("Risk: ", if_else(prediction$.pred_class == 1, 'low', 'high')),
             color = prediction_colour,
-            icon = icon("euro", lib = "glyphicon")
+            icon = icon("ok", lib = "glyphicon")
         )
     })
 
